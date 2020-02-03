132206b6019fa63513953d58134e0ab2c3bc4be1
hanli <hanl4@illinois.edu>
2020-02-03 16:34:12 -0500

    some tests

--- src/test/java/com/antra/phonepad/combination/CombinationApplicationTests.java
+++ src/test/java/com/antra/phonepad/combination/CombinationApplicationTests.java
@@ -3,6 +3,9 @@ package com.antra.phonepad.combination;
 import com.antra.phonepad.combination.Controller.CombinationController;
 import com.antra.phonepad.combination.Service.CombinationService;
 import com.antra.phonepad.combination.vo.NumberRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.mapper.ObjectMapperDeserializationContext;
+import io.restassured.mapper.ObjectMapperSerializationContext;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -14,12 +17,15 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 import static com.sun.org.apache.xml.internal.serializer.utils.Utils.messages;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
@@ -29,7 +35,9 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Matchers.anyObject;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -38,10 +46,22 @@ class CombinationApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
 
+
 	@Test
-	public void testComb() throws Exception{
+	public void getTest() throws Exception{
 		this.mockMvc.perform(get("/phonePad/test")).andExpect(status().isOk())
 				.andExpect(content().string(containsString("OK")));
+	}
+
+	@Test
+	public void getCombinationTest() throws Exception{
+		ObjectMapper mapper = new ObjectMapper();
+
+		this.mockMvc.perform(post("/phonePad/combination").content(mapper.writeValueAsString(new NumberRequest("217","8198","210")))
+		.contentType(MediaType.APPLICATION_JSON)
+		.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("message").value("Has Valid Combination"));
 	}
 
 }
